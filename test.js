4bc607413905d5a8916898586937a68243ec176e
Rodrigo Gomes da Silva <rodrigo.smscom@gmail.com>
2017-09-27 13:18:29 -0300

    Simple test

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
-// TODO
+const chai = require('chai');
+
+chai.should();
+
+const fastify = require('fastify')();
+
+const Router = require('./lib/router');
+
+describe('fastify-router', () => {
+  describe('Router', () => {
+    it('should return an object from the Router function', (done) => {
+      Router(fastify).should.be.an('object');
+      done();
+    });
+  });
+});
